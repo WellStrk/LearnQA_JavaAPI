b7d432f79708ba88cc3d4c643f9bd59f12c9764f
Valeria00 <astrakhantseva.00@mail.ru>
2026-04-27 21:29:01 +0300

    HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello World");
+    }
 }
