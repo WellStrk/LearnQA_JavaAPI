b0d8f914eb866e3e3ac13a04b7462001cea9b13d
Valeria00 <astrakhantseva.00@mail.ru>
2026-04-30 00:24:23 +0300

    Ex6: Редирект

--- src/test/java/Homework2.java
+++ src/test/java/Homework2.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class Homework2 {
     @Test
-    public void testGetText() {
+    public void testEx5() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .andReturn();
@@ -14,5 +14,20 @@ public class Homework2 {
         JsonPath jsonPath = response.jsonPath();
         String secondMessage = jsonPath.getString("messages[1].message");
         System.out.println("Текст второго сообщения: " + secondMessage);
+    }
+
+    @Test
+    public void testEx6() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        String FirstUrl = "https://playground.learnqa.ru/api/long_redirect";
+        System.out.println("Первый редирект на URL: " + FirstUrl);
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
     }
 }
