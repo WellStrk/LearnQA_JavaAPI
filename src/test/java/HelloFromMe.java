02579a37c04ce1d30f5f7dea8f8c7f30d0423622
Valeria00 <astrakhantseva.00@mail.ru>
2026-05-12 23:34:54 +0300

    Ex14: Формирование фреймворка

--- src/test/java/HelloFromMe.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class HelloFromMe{
-    @Test
-    public void testHelloWorld(){
-        System.out.println("Hello From Valeria");
-    }
-}
--- src/test/java/tests/HelloWorldTest.java
+++ src/test/java/tests/HelloWorldTest.java
@@ -1,12 +1,14 @@
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
     @Test
@@ -112,14 +114,43 @@ public class HelloWorldTest {
             cookies.put("auth_cookie", responseCookie);
         }
 
-        Response responseForCheck = RestAssured
-                .given()
+        Response responseForCheck = RestAssured.given()
                 .body(data)
                 .cookies(cookies)
                 .when()
                 .post("https://playground.learnqa.ru/api/check_auth_cookie")
                 .andReturn();
 
+
         responseForCheck.print();
     }
+
+    @Test
+    public void testRestPassed(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testHelloMethodWithoutName(String name){
+        Map<String,String> queryParams = new HashMap<>();
+
+        if(name.length() > 0){
+            queryParams.put("name", name);
+        }
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone";
+        assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
+    }
+
+
 }
