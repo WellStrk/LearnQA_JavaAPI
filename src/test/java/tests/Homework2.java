3a1d362e2f6ec6f68767cccd9e0309d980530d1a
Valeria00 <astrakhantseva.00@mail.ru>
2026-05-13 18:12:55 +0300

    Ex13: User Agent

--- src/test/java/tests/Homework2.java
+++ src/test/java/tests/Homework2.java
@@ -7,6 +7,8 @@ import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 import io.restassured.http.Headers;
 import io.restassured.http.Header;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -206,6 +208,34 @@ public class Homework2 extends BaseTestCase {
 
             assertNotNull(headerName, "headerName should not be null");
             assertNotNull(headerValue, "headerValue should not be null");
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', textBlock = """
+        "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" | Mobile | No | Android
+        "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1" | Mobile | Chrome | iOS
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" | Googlebot | Unknown | Unknown
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0" | Web | Chrome | No
+        "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1" | Mobile | No | iPhone
+    """)
+    public void Ex13(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice) {
+        Response response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check");
+
+        String actualPlatform = response.jsonPath().getString("platform");
+        String actualBrowser = response.jsonPath().getString("browser");
+        String actualDevice = response.jsonPath().getString("device");
+
+        if (!expectedPlatform.equals(actualPlatform) ||
+                !expectedBrowser.equals(actualBrowser) ||
+                !expectedDevice.equals(actualDevice)) {
+
+            System.out.println("Ошибка для User-Agent: " + userAgent);
+            System.out.println("Ожидаемые параметры: " + expectedPlatform + ", " + expectedBrowser + ", " + expectedDevice);
+            System.out.println("Полученые параметры: " + actualPlatform + ", " + actualBrowser + ", " + actualDevice);
         }
     }
 }
